33538be24ebd47436166ff17c61d8dc013d41766
Rishabh Dora <rishabhd.1121@gmail.com>
2024-03-19 00:26:02 +0530

    added jwt token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -48,6 +50,47 @@ const userSchema = new Schema(
 )
 
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+//making your own custom methods in mongoose
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)  //bcrypt library agar line 55 me password hash kr skti hai to yehi library password check bhi kr skti hai.
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName,
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_SECRET
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_SECRET
+        }
+    )
+}
 
 
 export const User = mongoose.model("User", userSchema);
