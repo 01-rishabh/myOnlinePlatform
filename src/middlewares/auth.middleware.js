bd41201b98041d91d83cfbba1d38db621f8db190
Rishabh Dora <rishabhd.1121@gmail.com>
2024-05-18 21:21:38 +0530

    authMiddleware added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,1 +1,27 @@
 //this middleware will just verify that the user is there or not.
+//middleware gets used in routes
+
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshtoken")
+
+    if(!user){
+        //nextvideo = discuss about frontend
+        throw new ApiError(401, "Invalid Access Token")
+    }
+
+    req.user = user;
+    next();
+})
