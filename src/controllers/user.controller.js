072495cd77b566564416234c421e76e316228a98
Rishabh Dora <rishabhd.1121@gmail.com>
2024-05-22 15:26:32 +0530

    Added comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -109,9 +109,14 @@ const loginUser = asyncHandler(async (req, res) => {
     const {email, username, password} = req.body;
 
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400, "Username or email is required.");
     }
+
+    //Here is an alternative of above code based on logic discussed
+    //if(!(username || email)){
+    //     throw new ApiError(400, "Username or email is required.")
+    //} 
 
     await User.findOne({
         $or:[{username}, {email}]
